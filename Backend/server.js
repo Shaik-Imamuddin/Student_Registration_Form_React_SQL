f52743992c9816ec0de52db56d3e59fca4880422
Immu <shaikimam37302@gmail.com>
2025-11-17 09:31:33 +0530

    Registration Form

---
+++ Backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Immu@9866",
+  database: "studentregdb",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL Connection Error:", err);
+    return;
+  }
+  console.log("MySQL Connected");
+});
+
+app.post("/api/students", (req, res) => {
+  const { regno, name, email, department } = req.body;
+
+  const query = `
+    INSERT INTO students (regno, name, email, department)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  db.query(query, [regno, name, email, department], (err, result) => {
+    if (err) {
+      return res.json({ success: false, message: err.message });
+    }
+    res.json({ success: true, message: "Student saved!" });
+  });
+});
+
+app.listen(2000, () => {
+  console.log("Server running on port 2000");
+});
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Student_Registration_Form_React_SQL
+
+Installation requirements:
+
+frontend:
+
+npm create-react-app frontend
+
+replace the src file
+
+backend:
+
+npm init -y
+npm install express mysql2 cors
+
+Schema:
+
+Database:
+
+create database studentregdb;
+
+Table:
+
+create table students(
+    regno int not null unique,
+    name varchar(100) not null,
+    email varchar(100) not null,
+    department varchar(50) not null
+);
